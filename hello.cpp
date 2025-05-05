0a0cb9b1baebf25c9375780b13d5bebd15f050ad
HGJustice <125309498+HGJustice@users.noreply.github.com>
2025-05-05 10:07:59 +0100

    messing around with c++ structs

--- hello.cpp
+++ hello.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
+#include <string>
+
+
+struct Person {
+    std::string name;
+    int age;
+    std::string nationality;
+};
+
+Person create_person(std::string _name, int _age, std::string _nationality) {
+   Person newPerson;
+   newPerson.name = _name;
+   newPerson.age = _age;
+   newPerson.nationality = _nationality;
+   return newPerson;
+}
+
+std::ostream& operator<<(std::ostream& os, const Person& person) {
+    os << "Name: " << person.name << "Age: " << person.age << "Nationality: " << person.nationality;
+    return os;
+}
 
 int main(){
-
-    int x;
-    std::cout << "Please enter a number" << '\n';
-    std::cin >> x;
-
-    // if (std::cin.fail()){
-    //     std::cout << "Please enter a number boiiii" << '\n';
-    //     return 1;
-    // }
-
-    std::cout << "the number " << x << '\n';
-
+    Person dennis = create_person("Dennis", 16, "ukarinian");
+    std::cout << dennis << "\n";
     return 0;
 }
