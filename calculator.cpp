05aeaed88cd583df14978e6aef2454de4db21718
HGJustice <125309498+HGJustice@users.noreply.github.com>
2025-05-10 13:18:35 +0100

    trying rto figure out calculator

--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <stack>
+#include <string>
+
+struct Operations {
+    std::string operation_symbol;
+    int precedent; 
+};
+
+void calculate(std::stack<int> _values, std::stack<Operations> _ops){
 
 
 
+}
 
 int main(){
-    std::cout << "hey" << "\n";
-    return 0;
+
+   std::stack<int> values;
+   std::stack<Operations>;
+   std::string expression;
+   std::getline(std::cin, expression);
+
+
+
+   return 0;
 }
